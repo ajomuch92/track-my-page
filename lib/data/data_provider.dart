49bb6b7c9ec24c19ba502f92b5b88ec8761c7875
Aarón Montes <aaron@kssco.com>
2021-06-03 21:22:51 -0600

    Updating provider to add methods to get quantity per country and per day

--- lib/data/data_provider.dart
+++ lib/data/data_provider.dart
@@ -2,6 +2,8 @@
 import 'package:supabase/supabase.dart';
 import 'package:track_my_page/data/supabase_client.dart';
 import 'package:track_my_page/models/counter_model.dart';
+import 'package:track_my_page/models/quantity_per_country.dart';
+import 'package:track_my_page/models/quantity_per_day.dart';
 
 class DataProvider {
   final SupabaseClient _client = clientSupabase;
@@ -17,5 +19,37 @@ class DataProvider {
       return CounterModel(count: data.length, avg: avg);
     }
     return CounterModel(count: 0, avg: 0);
+  }
+
+  Future<List<QuantityPerCountry>> getQuantityPerCountry() async {
+    List<QuantityPerCountry> list = [];
+    final result = await _client.from('vw_visitors_per_country').select().execute();
+    if(result.status == 200) {
+      List<dynamic> data = result.data;
+      for(var d in data) {
+        QuantityPerCountry quantity = QuantityPerCountry.fromJson(d);
+        list.add(quantity);
+      }
+    }
+    return list;
+  }
+
+  Future<List<QuantityPerDay>> getQuantityPerDay() async {
+    List<QuantityPerDay> list = [];
+    final result = await _client.from('vw_visitors_per_day')
+      .select()
+      .order('_year')
+      .order('_month')
+      .order('_day')
+      .limit(21)
+      .execute();
+    if(result.status == 200) {
+      List<dynamic> data = result.data;
+      for(var d in data) {
+        QuantityPerDay quantity = QuantityPerDay.fromJson(d);
+        list.add(quantity);
+      }
+    }
+    return list;
   }
 }
