58e422ade22bac1e711f79e41d5772b0c0a6afe1
Aarón Montes <aaron@kssco.com>
2021-06-04 20:52:04 -0600

    Updating quantity per day model and adding tables to graph views

--- lib/models/quantity_per_day.dart
+++ lib/models/quantity_per_day.dart
@@ -2,15 +2,22 @@
 import 'package:track_my_page/models/chart_model.dart';
 
 class QuantityPerDay{
-  const QuantityPerDay._({this.day, this.month, this.year, this.quantity,});
+  const QuantityPerDay._({this.day, this.month, this.year, this.quantity, this.date});
   final int? day;
   final int? month;
   final int? year;
-  final int? quantity; 
+  final int? quantity;
+  final DateTime? date;
 
   
   static QuantityPerDay fromJson(Map<String, dynamic> json){
-    return QuantityPerDay._(day:json['_day'], month:json['_month'], year:json['_year'], quantity:json['quantity'],);
+    return QuantityPerDay._(
+      day:json['_day'], 
+      month:json['_month'], 
+      year:json['_year'], 
+      quantity:json['quantity'],
+      date: DateTime(json['_year'] as int, json['_month'] as int, json['_day'] as int)
+    );
   }
 
   Map<String, dynamic> toJson(){
--- lib/views/graph.dart
+++ lib/views/graph.dart
@@ -16,6 +16,8 @@ class _GraphState extends State<Graph> with AutomaticKeepAliveClientMixin{
   final DataProvider provider = DataProvider();
   List<ChartModel> _pieData = [];
   List<ChartModel> _pieData2 = [];
+  List<QuantityPerCountry> _listQuantityPerCountry = [];
+  List<QuantityPerDay> _listQuantityPerDay = [];
 
   @override
   void initState() { 
@@ -32,23 +34,30 @@ class _GraphState extends State<Graph> with AutomaticKeepAliveClientMixin{
     }
     setState(() {
       _pieData = _data;
+      _listQuantityPerCountry = list;
     });
   }
   
   Future<void> initQuantityPerDay() async {
     List<QuantityPerDay> list = await provider.getQuantityPerDay();
     List<ChartModel> _data = [];
+    list.sort((a,b) => a.date!.compareTo(b.date!));
     for(var d in list) {
       _data.add(d.toChartModel());
     }
     setState(() {
       _pieData2 = _data;
+      _listQuantityPerDay = list;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     super.build(context);
+    Widget _separator = SizedBox(
+      height: 10.0,
+    );
+
     return Container(
       color: Colors.blueGrey.shade800,
       child: ListView(
@@ -58,10 +67,18 @@ class _GraphState extends State<Graph> with AutomaticKeepAliveClientMixin{
             title: 'Visitas por país',
             pieData: _pieData,
           ),
+          _separator,
+          _getTableViewPerCountry(),
+          _separator,
           ChartCard(
             title: 'Visitas por día',
             pieData: _pieData2,
           ),
+          _separator,
+          _getTableViewPerDay(),
+          SizedBox(
+            height: 100.0,
+          )
         ],
       ),
     );
@@ -69,4 +86,62 @@ class _GraphState extends State<Graph> with AutomaticKeepAliveClientMixin{
 
   @override
   bool get wantKeepAlive => true;
+
+  Widget _getTableViewPerCountry() {
+    List<TableRow> list = [
+      TableRow(
+        children: [
+          Column(children: [Text('País', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))]),
+          Column(children: [Text('Visitas', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))]),
+        ]
+      )
+    ];
+    for(QuantityPerCountry q in _listQuantityPerCountry) {
+      list.add(
+        TableRow(
+          children: [
+            Column(children: [Text(q.countryName!, style: TextStyle(color: Colors.white))]),
+            Column(children: [Text(q.quantity!.toString(), style: TextStyle(color: Colors.white))]),
+          ]
+        )
+      );
+    }
+    return Table(
+      border: TableBorder.all(  
+        color: Colors.white,  
+        style: BorderStyle.solid,  
+        width: 2
+      ),
+      children: list,
+    );
+  }
+
+  Widget _getTableViewPerDay() {
+    List<TableRow> list = [
+      TableRow(
+        children: [
+          Column(children: [Text('Día', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))]),
+          Column(children: [Text('Visitas', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold))]),
+        ]
+      )
+    ];
+    for(QuantityPerDay q in _listQuantityPerDay) {
+      list.add(
+        TableRow(
+          children: [
+            Column(children: [Text('${q.day}-${q.month}-${q.year}', style: TextStyle(color: Colors.white))]),
+            Column(children: [Text(q.quantity!.toString(), style: TextStyle(color: Colors.white))]),
+          ]
+        )
+      );
+    }
+    return Table(
+      border: TableBorder.all(  
+        color: Colors.white,  
+        style: BorderStyle.solid,  
+        width: 2
+      ),
+      children: list,
+    );
+  }
 }
